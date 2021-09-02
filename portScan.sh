7dd50e23db49d0ed625ee6eddbf55eff0fe24ddf
n3gox <85514196+n3gox@users.noreply.github.com>
2021-09-02 14:33:39 -0400

    Bash Script for port scan

---
+++ portScan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Control+C
+
+trap ctrl_c INT
+
+function ctrl_c(){
+        echo -e "\n[-] Exiting..."
+        tput cnorm; exit 1
+
+}
+
+
+# Main Port Scan
+
+read -e -p "Insert the ip to scan: " ipaddress
+
+for p in {1..65535}; 
+        do(echo >/dev/tcp/$ipaddress/$p) >/dev/null 2>&1 && echo "[+] Port $p OPEN"; 
+done
+
+echo -e "\n[!] The scan has finished"
