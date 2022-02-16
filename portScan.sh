fdd97e22e282c5fd9d9e0fa76103680dc4939738
n3gox <85514196+n3gox@users.noreply.github.com>
2022-02-16 21:33:47 +0100

    Better configuration

--- portScan.sh
+++ portScan.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Control+C
-trap ctrl_c INT
-
 function ctrl_c(){
         echo -e "\n[-] Exiting..."
         tput cnorm; exit 1
 }
 
+# Control+C
+trap ctrl_c INT
+
 # Main Port Scan
 
-read -e -p "Insert the ip to scan: " ipaddress
+ip=$1
 
-for p in {1..65535}; 
-        do(echo >/dev/tcp/$ipaddress/$p) >/dev/null 2>&1 && echo "[+] Port $p OPEN"; 
-done
+if [[  $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        for port in $(seq 1 65535); do
+                timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" 2>/dev/null && echo "[+] Port $port -> OPEN" &
+         done; wait
+else
+        echo -e "\n [!] Not a valid IP \n"
+        exit 1
+fi
 
-echo -e "\n[!] The scan has finished"
